9bc6ecdb396b261ff5dfb264ba9e2bcc84f996a1
sarahm <minutemin@gmail.com>
2023-07-24 18:26:13 -0700

    create thoughtRoutes.js and add connections to controller and psuedo code

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,8 @@
+// require express & router & thoughtController file
+const express = require('express');
+const router = express.Router();
+const thoughtController = require('../controllers/thoughtController');
+
+// Define your thought-related routes here using the thoughtController functions
+
+module.exports = router;
