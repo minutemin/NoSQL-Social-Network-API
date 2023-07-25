19cda378818541e5021a2f1fcebe4fa7b9eef884
sarahm <minutemin@gmail.com>
2023-07-24 18:19:21 -0700

    add getOneUser and createUser functions in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,24 @@ export const getAllUsers = async(req, res) => {
     console.log(err);
   }
 }
+
+export const getOneUser = async(req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId });
+    if (!user) {
+      res.status(404).json("User is not found");
+    } 
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (err) {
+    console.log(err);
+  }
+}
