04be8f99d2e20c2cc43ce27d24dc43828f79003d
sarahm <minutemin@gmail.com>
2023-07-24 18:27:35 -0700

    create start basics with reactionRoutes and some psuedo notes

---
+++ routes/api/reactionRoutes.js
@@ -0,0 +1,8 @@
+// require express & router * thoughtController
+const express = require('express');
+const router = express.Router();
+const thoughtController = require('../controllers/thoughtController');
+
+// Define your reaction-related routes here using the thoughtController functions
+
+module.exports = router;
